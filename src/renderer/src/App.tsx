d41ac669fa1a8b91926108c3b96efde92a95a892
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-05-11 18:29:04 -0300

    format

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
 
 function App(): JSX.Element {
   return (
     <>
       <h1>App</h1>
       <Button>click</Button>
+      <Input type="text" />
     </>
   )
 }
