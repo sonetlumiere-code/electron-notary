c67aded6fef7f719c62ff7e6c7da782eb3e6437f
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-05-11 18:00:47 -0300

    shadcn config

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from './components/ui/button'
+
 function App(): JSX.Element {
-  return <h1>App</h1>
+  return (
+    <>
+      <h1>App</h1>
+      <Button>click</Button>
+    </>
+  )
 }
 
 export default App
