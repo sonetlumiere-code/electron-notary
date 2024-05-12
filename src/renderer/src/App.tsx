28f5ba66c56a6e29168a040ecdc7462bc4cf4825
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-05-12 16:46:49 -0300

    layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,20 @@
+import Nav from './components/layout/nav'
 import { Button } from './components/ui/button'
 import { Input } from './components/ui/input'
 
 function App(): JSX.Element {
   return (
-    <>
-      <h1>App</h1>
-      <Button>click</Button>
-      <Input type="text" />
-    </>
+    <div className="flex flex-col h-full w-full">
+      <div className="flex flex-1 overflow-hidden">
+        <aside className="w-64 border-r overflow-auto">
+          <Nav />
+        </aside>
+        <main className="flex-1 overflow-auto min-h-[86vh] p-6 lg:p-8 bg-muted/40">
+          <Button>click</Button>
+          <Input type="text" />
+        </main>
+      </div>
+    </div>
   )
 }
 
---
+++ src/renderer/src/types/index.ts
@@ -0,0 +1,71 @@
+// Define the document type enum
+export enum DocumentType {
+  DNI = 'DNI',
+  LC = 'LC',
+  LE = 'LE',
+  PASAPORTE = 'PASAPORTE'
+}
+
+export enum MaritalStatus {
+  SOLTERO = 'SOLTERO',
+  CASADO = 'CASADO',
+  DIVORCIADO = 'DIVORCIADO',
+  VIUDO = 'VIUDO'
+}
+
+export interface MaritalStatusDetails {
+  spouseName?: string
+  spouseNumber?: number
+  marriageRegime?: string
+  divorceNumber?: number
+  divorceDate?: Date
+  divorceCourt?: string
+  divorceAutos?: string
+  deceasedSpouseName?: string
+}
+
+export interface PersonDataSheet {
+  id: string
+  name: string
+  lastName: string
+  gender: string
+  nationality: string
+  documentType: DocumentType
+  documentNumber: number
+  CUIT_L: number
+  birthdate: Date
+  birthplace: string
+  maritalStatus: MaritalStatus
+  maritalStatusDetails?: MaritalStatusDetails
+  numberOfChildren?: number
+  address: string
+  city: string
+  province: string
+  profession: string
+  phoneNumber: string
+  mobileNumber: string
+  email: string
+  isPoliticallyExposed?: boolean
+  politicalPosition?: string
+  originOfFunds?: string
+  reasonForChoosing: string
+  referredBy?: string
+}
+
+export type LegalPersonDataSheet = {
+  id: string
+  businessName: string
+  CUIT: number
+  legalAddress: string
+  mainActivity: string
+  instrumentOfConstitution: string
+  registrationDate: Date
+  registrationNumber: number
+  registeredOfficePhone: number
+  registeredOfficeAddress: string
+  registeredOfficeEmail: string
+  statuteCopy: string
+  proceedingsCopy: string
+  balanceCopy: string
+  representativeData: string
+}
