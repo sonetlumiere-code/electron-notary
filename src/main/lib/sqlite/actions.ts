53c1548d6fe3acb2db900b6132feedfcb387b300
Lucas Perez <LUPEREZ@TECO.COM.AR>
2024-05-20 11:57:19 -0300

    sqlite3 CRUD

--- src/main/lib/sqlite/actions.ts
+++ src/main/lib/sqlite/actions.ts
@@ -34,4 +34,53 @@ const getUsers = async () => {
   })
 }
 
-export { createUser, getUsers }
+const getUserById = async (id: number) => {
+  const query = `SELECT * FROM users WHERE id = ?`
+
+  return new Promise((resolve, reject) => {
+    db.get(query, [id], (err, row) => {
+      if (err) {
+        console.error('Error retrieving data: ', err)
+        reject(err)
+      } else {
+        resolve(row)
+      }
+    })
+  })
+}
+
+const updateUser = async ({ id, name, email }: { id: number; name: string; email: string }) => {
+  const query = `UPDATE users SET name = ?, email = ? WHERE id = ?`
+
+  return new Promise((resolve, reject) => {
+    db.run(query, [name, email, id], function (err) {
+      if (err) {
+        console.error('Error updating data: ', err)
+        reject(err)
+      } else {
+        resolve({
+          id,
+          name,
+          email
+        })
+      }
+    })
+  })
+}
+
+const deleteUser = async (id: number) => {
+  const query = `DELETE FROM users WHERE id = ?`
+
+  return new Promise((resolve, reject) => {
+    db.run(query, [id], function (err) {
+      if (err) {
+        console.error('Error deleting data: ', err)
+        reject(err)
+      } else {
+        resolve({ id })
+      }
+    })
+  })
+}
+
+export { createUser, deleteUser, getUserById, getUsers, updateUser }
