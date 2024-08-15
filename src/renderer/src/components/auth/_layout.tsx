cc5ce5221574d91ab8052b45b4397f245aa03c10
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-08-14 22:43:11 -0300

    auth page logo

--- src/renderer/src/components/auth/_layout.tsx
+++ src/renderer/src/components/auth/_layout.tsx
@@ -1,16 +1,17 @@
-import { Feather } from "lucide-react"
 import { Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-3 text-center">
-          <Feather className="mx-auto w-32 h-32 py-5" />
+        <div className="flex flex-col space-y-6 text-center">
+          <div className="px-24 w-full">
+            <img src="src/assets/img/logo_etchart.png" alt="Logo Etchart" />
+          </div>
 
-          <h1 className="text-2xl font-semibold tracking-tight">Ingresar a tu cuenta</h1>
+          {/* <h1 className="text-2xl font-semibold tracking-tight">Ingresar a tu cuenta</h1> */}
           <p className="text-sm text-muted-foreground">
-            Introduce tus credenciales para iniciar sesión
+            Ingresa tus credenciales para iniciar sesión
           </p>
         </div>
 
